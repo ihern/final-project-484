1b38fddd3b14d0090c9eb75f1ef36cca4b8048ae
Isaias <isaias64hernandez@gmail.com>
2023-10-09 22:20:39 -0500

    Implement a GUI for the login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 
-import './App.css'
+
+import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import MVP from './components/MVP';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      <h1>Speed Dating App</h1>
-          <img src="../images/exacalidraw.png" className="logo" alt="Excali diagram drawing" />
-      </div>
-      <div className="card">
-        <a rel="stylesheet" href="https://excalidraw.com/#json=8PE0U9KuKveDcR-_TCkfI,y0evdrK57JE8g_eJkwchBA"><h1>Excalidraw Link</h1></a>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <LoginPage />} />
+          <Route path="/mvp" element={ <MVP />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }

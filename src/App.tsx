de9f75b3e1c84d551f133764dedae39b46d159b8
Isaias <isaias64hernandez@gmail.com>
2023-10-14 12:22:44 -0500

    Start implementation of client questionaire

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import MVP from './components/MVP';
+import PersonalSurvey from './components/PersonalSurvey';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={ <LoginPage />} />
           <Route path="/mvp" element={ <MVP />} />
+          <Route path="/survey" element={ <PersonalSurvey />} />
         </Routes>
       </BrowserRouter>
     </>
